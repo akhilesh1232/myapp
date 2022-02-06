d4843294538bc24273414f724dd92dfb06e13ab4
akhilesh1232 <74225719+akhilesh1232@users.noreply.github.com>
2022-02-06 09:47:17 +0530

    layout changed

--- src/Blogs/Blog2.jsx
+++ src/Blogs/Blog2.jsx
@@ -5,18 +5,19 @@ function Blog2(){
     return(
         <>
             <div className="container-fluid">
-                <div className="row">
-                    <div className="col-10 mx-auto">
                         <div className="row">
-                        <h1 className = "mx-auto" id="heading">
-                    Indian Education System
-            </h1>
-            <p id="article"> <span className="tab"></span>“The future of nation is in the hands of youth”. As the slogan itself is mentioning the youth, it is concerned to education. And in this blog, I’m going to write my personal opinions about the current education system in India.<br /><span className="tab"></span>
+                            <h1 className = "my-3 my-5 mx-auto" id="heading">
+                                Indian Education System
+                            </h1>
+                        </div>
+                        <div className="row">
+                            <div className="col-10 mx-auto">
+                            <p id="article"> <span className="tab"></span>“The future of nation is in the hands of youth”. As the slogan itself is mentioning the youth, it is concerned to education. And in this blog, I’m going to write my personal opinions about the current education system in India.<br /><span className="tab"></span>
 The Indian community has a great educational heritage. Our ancestors have written great Geeta, Ramayana, Vedas, Mahabharata, Upanishad and many more novels and plays, to bring about changes in the society. And for many generations, it was taught as part of syllabus. So we have a great historical background on education. Now looking at today’s education system, it is said that we are losing all our knowledge and wisdom for the word named “marks”. Colleges are trying to produce scholars in the sense of highest marks. I believe that some times the marks are telling your whole story but, “some times”. We often hear statements like “a single piece of paper can’t decide my future”. But from another point of view, you can’t rely only on your soft skills. Similarly there is no strong answer to the question that what’s wrong if you get good marks. You need to get good marks to prove yourself. So I have the humble opinion that everyone should maintain the marks since it’s not in our hands to change the marking systems.<br /><span className="tab"></span>Another point on which I want your attention to is, primary and secondary education. Government is trying to focus on the overall development of children but on the ground level, it is not working. Still, parents want their children to study. They are putting sports and other soft skills after study. It is okay to make your children study, but forcing them only to study and not to let them do other things is somewhat wrong. I can’t totally blame this for the education system but some efforts must be done by the system on this and I must say the mindset of parents is changing now days.<br /><span className="tab"></span>Continuing the above problem, we also need to take a look at the recruitment of teachers in universities and schools. If a master is not perfect in his job then how you can expect a student to be perfect in his job? So, the class of teacher teaching must be improved in every college and school. Every time you can’t point on students for not attending lectures and scoring poor marks there must be something mistake from other side. Sometimes it’s the situation that the teacher is an expert in the subject but due to poor communication skills and lack of interest in teaching makes him a boring ‘lecturer’.<br /><span className="tab"></span>Although, there is not everything going wrong in our education system, there are many good things. The syllabus of central and state boards is good enough for students to learn ethics and many more things that teach you to how survive in the world. The most valuable asset of our system is maths. All across the world, it is proved that Indians are good mathematicians, they know how to solve difficult problems in an easy way. Another great thing to count, is that students are now more excited about the entrepreneurshipc which make them more mature and able to think of the world in better way. <br /><span className="tab"></span>At last, the role of education in life is to make a child a good person with knowledge with which it can survive in competition, and help to make this world a better place. And till now our education system has done this job incredibly. But still, there is scope to improve. And let’s dream the world of tomorrow, with great personalities all around us in India and with help of which we can bring our honour of "Vishvguru" back.</p>
+                        
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
         </>
     );
 }
